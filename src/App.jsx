8a39a49aa150c2efe7fef09d442164bfc121fc78
ibrahim-khalil0 <mdibrahimkhalil0183@gmail.com>
2023-09-15 19:14:24 +0600

    header section compleate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Courses from './components/Courses/Courses'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="w-[90%] mx-auto py-10">
+            <h1 className="text-center text-[#1C1B1B] font-bold text-4xl">Course Registration</h1>
+        </header>
+    );
+};
+
+export default Header;
